24f87d9705152bd75b1a0f82bdd867687dadaa80
Volgar04 <nicolasmartin0404@gmail.com>
2019-01-16 19:14:12 +0100

    Test requête réseau (API NYT)

--- app/src/main/java/com/nicolappli/mynews/Controllers/Fragments/TopStoriesPageFragment.java
+++ app/src/main/java/com/nicolappli/mynews/Controllers/Fragments/TopStoriesPageFragment.java
@@ -12,24 +12,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.nicolappli.mynews.Adapters.ArticleAdapter;
 import com.nicolappli.mynews.R;
+import com.nicolappli.mynews.Utils.NetworkAsyncTask;
 import com.nicolappli.mynews.Views.ArticleItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TopStoriesPageFragment extends Fragment {
+public class TopStoriesPageFragment extends Fragment implements NetworkAsyncTask.Listeners {
 
     Context thisContext;
     RecyclerView recyclerView;
     ArticleAdapter adapter;
 
     List<ArticleItem> articleList;
+
+    @BindView(R.id.test11)
+    TextView textview;
 
     public TopStoriesPageFragment() { }
 
@@ -40,18 +49,50 @@ public class TopStoriesPageFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        thisContext=container.getContext();
+//        thisContext=container.getContext();
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_top_stories_page, container, false);
+        ButterKnife.bind(this, rootView);
 
-        articleList = new ArrayList<>();
+//        articleList = new ArrayList<>();
 
-        recyclerView = rootView.findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true); // Set the size of the RecyclerView has fixed
+        //recyclerView = rootView.findViewById(R.id.recyclerView);
+//        recyclerView.setHasFixedSize(true); // Set the size of the RecyclerView has fixed
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(thisContext));
+//        recyclerView.setLayoutManager(new LinearLayoutManager(thisContext));
 
         return rootView;
     }
 
+    @OnClick(R.id.test11btn)
+    public void submit(View view){
+        this.executeHttpRequest();
+    }
+
+    private void executeHttpRequest(){
+        new NetworkAsyncTask(this).execute("https://api.nytimes.com/svc/mostpopular/v2/emailed/1.json?api-key=AHGAejtcPdRPUyAnADLIR4H6g7nLW4E6");
+    }
+
+    @Override
+    public void onPreExecute() {
+        this.updateUIWhenStartingHTTPRequest();
+    }
+
+    @Override
+    public void doInBackground() {
+
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        this.updateUIWhenStopingHTTPRequest(json);
+    }
+
+    private void updateUIWhenStartingHTTPRequest(){
+        this.textview.setText("Downloading...");
+    }
+
+    private void updateUIWhenStopingHTTPRequest(String response){
+        this.textview.setText(response);
+    }
 }
